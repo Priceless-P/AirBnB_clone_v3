4af627a56cb6b5db3014b2f7fc9d811cdf56d1a3
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-02-05 15:15:29 +0100

    correction

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,18 +2,18 @@
 """ Cities API
 """
 
-from api.v1.views import app_views
-from models import storage
-from models.city import City
-from models.state import State
 from flask import abort
-from flask import jsonify
+from api.v1.views import app_views
+from models.city import City
 from markupsafe import escape
-from flask import request
+from flask import jsonify
 from flask import make_response
+from flask import request
+from models.state import State
+from models import storage
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
+@app_views.route('/states/<string:state_id>/cities', methods=['GET', 'POST'])
 def get_create_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, escape(state_id))
@@ -25,15 +25,15 @@ def get_create_cities(state_id):
             for city in state.cities:
                 city_ = city.to_dict()
                 all_cities.append(city_)
-            return jsonify(all_cities)
+            return make_response(jsonify(all_cities))
     elif request.method == 'POST':
         data = request.get_json()
         name = data.get('name')
         print(data)
         if not data:
-            abort(404, 'Not a JSON')
+            abort(404, "Not a JSON")
         if not name:
-            abort(404, 'Missing name')
+            abort(404, "Missing name")
         if not state:
             abort(404)
 
@@ -42,14 +42,14 @@ def get_create_cities(state_id):
         return make_response(jsonify(new_city.to_dict()), 201)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('/cities/<string:city_id>', methods=['GET', 'DELETE', 'PUT'])
 def city(city_id):
     """Retrieves, Modifies or Deletes a City object"""
     city = storage.get(City, escape(city_id))
     if city is None:
         abort(404)
     if request.method == 'GET':
-        return jsonify(city.to_dict())
+        return make_response(jsonify(city.to_dict()))
     elif request.method == 'DELETE':
         storage.delete(city)
         storage.save()
@@ -57,7 +57,7 @@ def city(city_id):
     elif request.method == 'PUT':
         data = request.get_json()
         if not data:
-            abort(400, 'Not a JSON')
+            abort(400, "Not a JSON")
         for key, value in data.items():
             if key not in ('id', 'created_at', 'updated_at'):
                 setattr(city, key, value)
